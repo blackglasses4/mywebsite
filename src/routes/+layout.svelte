999201c064585854ee7584f77ef247e3edc811b9
Adrianna Pawłowska <blackglassespx@gmail.com>
2023-07-14 11:14:10 +0200

    zmiany w link, layout.svelte, css i svelte menu, dodanie zdjęć.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,21 @@
 <script>
-    import '$lib/styles/style.css';
+	import '$lib/styles/style.css';
+	import { page } from '$app/stores';
 </script>
 
-<header>
-	<a href="/">Home</a>
-	<nav>
-		<ul>
-			<li><a href="/blog">Blog</a></li>
-			<li><a href="/portfolio">Portfolio</a></li>
-			<li><a href="/about">O mnie</a></li>
-			<li><a href="/contact">Kontakt</a></li>
+<div class="container">
+	<header>
+		<a id="left-header" href="/">adrianna pawłowska</a>
+		<ul class="right-header">
+			<li><a href="/about" class:active={$page.url.pathname == '/about/'}>o mnie</a></li>
+			<li><a href="/portfolio" class:active={$page.url.pathname == '/portfolio/'}>portfolio</a></li>
+			<li><a href="/contact" class:active={$page.url.pathname == '/contact/'}>kontakt</a></li>
 		</ul>
-	</nav>
-</header>
+	</header>
+	
+	<main>
+		<slot />
+	</main>
 
-<main>
-	<slot />
-</main>
-
-<footer>&copy; 2023 kpawlx</footer>
+	<footer>&copy; 2023 by Adrianna. All rights reversed.</footer>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,12 @@
-<h1>Witam</h1>
+<section class="main-home">
+    <div class="main-introduce">
+        <h2>Projektant 3D</h2>
+        <h4>oraz pełny przygód</h4>
+        <h2>Frontend Developer,</h2>
+        <h4>którego ciągnie do tworzenia unikalnych i rzucających się w oczy projektów.</h4>
+        <button><span>Zobacz prace</span></button>
+    </div>
+</section>
+<section class="introduce">
+    
+</section>
