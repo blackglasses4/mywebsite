8f6ab3990a06e05be3e0bd7274d37d70017f816b
Adrianna Pawłowska <blackglassespx@gmail.com>
2024-05-03 23:09:28 +0200

    Zrobiony section aboutme, project, skill, końcówka footer

--- src/routes/pl/+layout.svelte
+++ src/routes/pl/+layout.svelte
@@ -31,9 +31,10 @@
 
 		<nav class="nav">
 			<ul class="nav__menu">
-				<li><a href="/{lang}/offer/" id="offer" rel="internal" class:active={$page.url.pathname == '/'+lang+'/offer/'}>Oferta</a></li>
-				<li><a href="/{lang}/#aboutme" id="about" rel="internal" class:active={$page.url.pathname == '/'+lang+'/about'}>O nas</a></li>
-				<li><a href="/{lang}/contact/" id="contact" rel="internal" class:active={$page.url.pathname == '/'+lang+'/contact/'}>Kontakt</a></li>
+				<li><a href="/{lang}/#aboutme" rel="internal" class:active={$page.url.pathname == '/'+lang+'/about'}>Krótko o mnie</a></li>
+				<li><a href="/{lang}/#skill" rel="internal" class:active={$page.url.pathname == '/'+lang+'/skill'}>Umiejętności</a></li>
+				<li><a href="/{lang}/#project" rel="internal" class:active={$page.url.pathname == '/'+lang+'/project'}>Projekty</a></li>
+				<li><a href="/{lang}/#contact" rel="internal" class:active={$page.url.pathname == '/'+lang+'/contact/'}>Kontakt</a></li>
 			</ul>
 			<div class="nav__icon">
 				<p><a target="_blank" aria-label="Anchor to transfer to the company Facebook page" href="https://www.facebook.com/TelmechSpolka" rel="noopener noreferrer"><i class='bx bxl-facebook'></i></a></p>
@@ -47,39 +48,67 @@
 	<slot />
 </main>
 
-<!-- <footer>
-	<div class="footer-areas">
-		<div class="footer-area footer-left">
-			//<img src="/image/logo.png" alt="Logo firmy Telmech Sp. z.o.o."> 
-			<div class="footer-address">
-				<p>ul. Bolesława Chrobrego 6</p>
-				<p>64-500 Szamotuły</p>
-				<p>Polska</p>
-			</div>
-			<div class="footer-nip">
-				<p>NIP: 781&ndash;00&ndash;64&ndash;886</p>
-				<p>REGON: 630226676</p>
-			</div>
-			<div class="footer-app-icon">
+<footer id="contact">
+	<h1>Chcesz rozpocząć wspólny projekt?</h1><hr>
+	<div class="footer__content">
+		<div class="footer__content-call">
+			<h2>Napisz do mnie!</h2>
+			<p><a href="mailto:adriannapx@gmail.com">adriannapx@gmail.com</a></p>
+			<p>+48 730 464 132</p>
+		</div>
+		<div class="footer__content-nav">
+			<h2>Navigacja</h2>
+			<p><a href="/{lang}/#aboutme" rel="internal" class:active={$page.url.pathname == '/'+lang+'/about'}>Krótko o mnie</a></p>
+			<p><a href="/{lang}/#skill" rel="internal" class:active={$page.url.pathname == '/'+lang+'/skill'}>Umiejętności</a></p>
+			<p><a href="/{lang}/#project" rel="internal" class:active={$page.url.pathname == '/'+lang+'/project'}>Projekty</a></p>
+		</div>
+	</div>
+	<div class="footer__copysocial">
+		<h3>Socials</h3>
+		<div class="footer__copy">
+			<p>&copy; 2024 by Telmech Sp. z.o.o. Wszelkie prawa zastrzeżone &vert; Wykonanie <a target="_blank"
+			href="https://adrianna.mooo.com/" rel="noopener noreferrer">Adrianna Pawłowska</a></p>
+
+			<div class="footer-social">
 				<p><a target="_blank" aria-label="Anchor to transfer to the company Facebook page" href="https://www.facebook.com/TelmechSpolka" rel="noopener noreferrer"><i class='bx bxl-facebook'></i></a></p>
-				//<p><a target="_blank" aria-label="Anchor to transfer to the company Linkedln page" href="/"><i class='bx bxl-linkedin-square' ></i></a></p
+				<p><a target="_blank" aria-label="Anchor to transfer to the company Linkedln page" href="/"><i class='bx bxl-linkedin-square' ></i></a></p>
 				<p><a target="_blank" aria-label="Anchor to transfer to the company Instagram page" href="https://www.instagram.com/telmechspolkazoo/" rel="noopener noreferrer"><i class='bx bxl-instagram-alt'></i></a></p>
-			</div>
-		</div>
-		<div class="footer-area footer-center">
-			<h1><a class="footer-a" href="/{lang}/offer" rel="noopener noreferrer internal">Oferta</a></h1>
-			<p><a class="p-a" href="/{lang}/offer#details" rel="noopener noreferrer internal">Detale i konstrukcje z blach cienkich</a></p>
-			<p><a class="p-a" href="/{lang}/offer#constructions" rel="noopener noreferrer internal">Konstrukcje zgrzewane i spawane</a></p>
-			<p><a class="p-a" href="/{lang}/offer#locksmithWorks" rel="noopener noreferrer internal">Precyzyjne prace ślusarskie i montażowe</a></p>
-			<p><a class="p-a" href="/{lang}/offer#galvanicCoatings" rel="noopener noreferrer internal">Powłoki galwaniczne i malowanie proszkowe</a></p>
-		</div>
-		<div class="footer-area footer-right">
-			<h1><a class="footer-a" href="/{lang}/contact" rel="noopener noreferrer internal">Kontakt</a></h1>
-			<p>Biuro Handlowe: +48 61 868 03 83</p>
-			<p>Sekretariat: +48 61 868 59 49</p>
+			</div>	
 		</div>
 	</div>
-	<div class="footer-text">
-		<p>&copy; 2024 by Telmech Sp. z.o.o. Wszelkie prawa zastrzeżone &vert; Wykonanie <a target="_blank" href="https://adrianna.mooo.com/" rel="noopener noreferrer">Adrianna Pawłowska</a></p>
+</footer>
+
+<!-- <div class="footer-areas">
+	<div class="footer-area footer-left">
+		<img src="/image/logo.png" alt="Logo firmy Telmech Sp. z.o.o."> 
+		<div class="footer-address">
+			<p>ul. Bolesława Chrobrego 6</p>
+			<p>64-500 Szamotuły</p>
+			<p>Polska</p>
+		</div>
+		<div class="footer-nip">
+			<p>NIP: 781&ndash;00&ndash;64&ndash;886</p>
+			<p>REGON: 630226676</p>
+		</div>
+		<div class="footer-app-icon">
+			<p><a target="_blank" aria-label="Anchor to transfer to the company Facebook page" href="https://www.facebook.com/TelmechSpolka" rel="noopener noreferrer"><i class='bx bxl-facebook'></i></a></p>
+			<p><a target="_blank" aria-label="Anchor to transfer to the company Linkedln page" href="/"><i class='bx bxl-linkedin-square' ></i></a></p>
+			<p><a target="_blank" aria-label="Anchor to transfer to the company Instagram page" href="https://www.instagram.com/telmechspolkazoo/" rel="noopener noreferrer"><i class='bx bxl-instagram-alt'></i></a></p>
+		</div>
 	</div>
-</footer> -->
+	<div class="footer-area footer-center">
+		<h1><a class="footer-a" href="/{lang}/offer" rel="noopener noreferrer internal">Oferta</a></h1>
+		<p><a class="p-a" href="/{lang}/offer#details" rel="noopener noreferrer internal">Detale i konstrukcje z blach cienkich</a></p>
+		<p><a class="p-a" href="/{lang}/offer#constructions" rel="noopener noreferrer internal">Konstrukcje zgrzewane i spawane</a></p>
+		<p><a class="p-a" href="/{lang}/offer#locksmithWorks" rel="noopener noreferrer internal">Precyzyjne prace ślusarskie i montażowe</a></p>
+		<p><a class="p-a" href="/{lang}/offer#galvanicCoatings" rel="noopener noreferrer internal">Powłoki galwaniczne i malowanie proszkowe</a></p>
+	</div>
+	<div class="footer-area footer-right">
+		<h1><a class="footer-a" href="/{lang}/contact" rel="noopener noreferrer internal">Kontakt</a></h1>
+		<p>Biuro Handlowe: +48 61 868 03 83</p>
+		<p>Sekretariat: +48 61 868 59 49</p>
+	</div>
+</div>
+<div class="footer-text">
+	<p>&copy; 2024 by Telmech Sp. z.o.o. Wszelkie prawa zastrzeżone &vert; Wykonanie <a target="_blank" href="https://adrianna.mooo.com/" rel="noopener noreferrer">Adrianna Pawłowska</a></p>
+</div> -->
--- src/routes/pl/+page.svelte
+++ src/routes/pl/+page.svelte
@@ -3,6 +3,7 @@
     import { onMount } from 'svelte';
     // import InfinitiveLogos from '$lib/components/InfinitiveLogos.svelte';
     import aboutme from '$lib/assets/aboutme.jpg?as=run'
+    import svelte from '$lib/assets/svelte.png?as=run'
     // import homeBanerTwo from '$lib/assets/main-baner-1.jpg?as=run'
     // import certyfikat from '$lib/assets/certyfikat.jpg?as=run'
 
@@ -28,6 +29,81 @@
             oraz tworzeniem nowoczesnych <strong>stron internetowych</strong> pochodzący z Poznania, z Polski.</h1>
             <p>Studiuję Informatykę na trzecim roku na uniwersytecie MERITO WSB w Poznaniu.
             Poszukuję stażu lub pracy, która pozwoli mi poszerzyć horyzonty i odkryć nowe możliwości.</p>
+        </div>
+    </div>
+</section>
+
+<section id="skill" class="section__skill">
+    <div class="section__skill-content">
+        <h1>Moje umiejętności</h1>
+        <p>Zdobyłam wiedzę w zakresie projektowania świata dwuwymiarowego jak i trójwymiarowego z użyciem 
+        narzędzi takich jak: Blender, Gimp, Inkscape oraz Krita.</p>
+        <p>Zaś przy projektowaniu oraz programowaniu stron internetowych zdobyłam biegłość w posługiwaniu
+         się technologiami takimi jaki: HTML, CSS, Sass, JavaScript oraz Figma.</p>
+    </div>
+</section>
+
+<section id="project" class="section__project">
+    <div class="section__project-content">
+        <h1>Wyróżnione Projekty</h1>
+
+        <div class="projects">
+            <h2>Telmech</h2><hr>
+            <div class="projects__details">
+                <div class="projects__details-lang">
+                    <p>UI Design &vert; Web Development <span class="green-data">2024</span></p>
+                    <i class='bx bxl-html5'></i>
+                    <i class='bx bxl-css3' ></i>
+                    <i class='bx bxl-javascript'></i>
+                    <i class='bx bxl-figma' ></i>
+                </div>
+                <div class="projects__details-lang">
+                    <div class="details-pages">
+                        <div class="details-page">
+                            <i class='bx bx-world'></i>
+                            <p><a target="_blank" aria-label="Anchor to be moved to the telmech site"
+                            href="https://www.telmech.com.pl" rel="noopener noreferrer"
+                        >Website</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="projects__img">
+                <Img class="project-img" src={aboutme} alt="Zdjęcie przedstawiające mnie" />
+            </div>
+        </div>
+
+        <div class="projects projects-next">
+            <h2>Moja strona portfolio</h2><hr>
+            <div class="projects__details">
+                <div class="projects__details-lang">
+                    <p>UI Design &vert; Web Development <span class="green-data">2024</span></p>
+                    <i class='bx bxl-html5'></i>
+                    <i class='bx bxl-css3' ></i>
+                    <i class='bx bxl-sass' ></i>
+                    <i class='bx bxl-javascript'></i>
+                    <i class='bx bxl-figma' ></i>
+                </div>
+                <div class="projects__details-lang">
+                    <div class="details-pages">
+                        <div class="details-page">
+                            <i class='bx bx-world'></i>
+                            <p><a target="_blank" aria-label="Anchor to be moved to the telmech site"
+                            href="https://adrianna.mooo.com/"
+                            rel="noopener noreferrer" >Website</a></p>
+                        </div>
+                        <div class="details-page">
+                            <i class='bx bxl-github' ></i>
+                            <p><a target="_blank" aria-label="Anchor to transfer to the
+                            company Facebook page" href="https://github.com/blackglasses4/mywebsite"
+                            rel="noopener noreferrer" >Source Code</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="projects__img">
+                <Img class="project-img" src={aboutme} alt="Zdjęcie przedstawiające mnie" />
+            </div>
         </div>
     </div>
 </section>
