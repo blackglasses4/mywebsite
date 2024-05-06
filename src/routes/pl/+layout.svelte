84f58b7ec9c43317ecc524fa83837c7c9f4641e3
Adrianna Pawłowska <blackglassespx@gmail.com>
2024-05-06 14:39:49 +0200

    footer naprawiony

--- src/routes/pl/+layout.svelte
+++ src/routes/pl/+layout.svelte
@@ -66,17 +66,13 @@
 		</div>
 	</div>
 	<div class="footer__copysocial">
-		<div class="footer__copy">
-			<div class="footer-copy">
-				<p>&copy; 2024 by Adrianna Pawłowska </p>
-				<p>Strona zaprojektowana i zakodowana z &#x2764;&#xFE0F;&#x200D;&#x1F525;</p>
-			</div>
-			<div class="footer-social">
-				<p><a target="_blank" aria-label="Anchor to transfer to the company Facebook page" href="https://www.facebook.com/profile.php?id=100013368822697" rel="noopener noreferrer"><i class='bx bxl-facebook-square' ></i></a></p>
-				<p><a target="_blank" aria-label="Anchor to transfer to the company Linkedln page" href="https://www.linkedin.com/in/adrianna-paw%C5%82owska-a24850215/"><i class='bx bxl-linkedin-square' ></i></a></p>
-				<p><a target="_blank" aria-label="Anchor to transfer to the company Instagram page" href="https://www.instagram.com/zielona_laska/" rel="noopener noreferrer"><i class='bx bxl-instagram-alt'></i></a></p>
-				<p><a target="_blank" aria-label="Anchor to transfer to the company Github page" href="https://github.com/blackglasses4/mywebsite" rel="noopener noreferrer"><i class='bx bxl-github' ></i></a></p>
-			</div>
+			<p>&copy; 2024 by Adrianna Pawłowska </p>
+			<p>Strona zaprojektowana i zakodowana z &#x2764;&#xFE0F;&#x200D;&#x1F525;</p>
+		<div class="footer-social">
+			<p><a target="_blank" aria-label="Anchor to transfer to the company Facebook page" href="https://www.facebook.com/profile.php?id=100013368822697" rel="noopener noreferrer"><i class='bx bxl-facebook-square' ></i></a></p>
+			<p><a target="_blank" aria-label="Anchor to transfer to the company Linkedln page" href="https://www.linkedin.com/in/adrianna-paw%C5%82owska-a24850215/"><i class='bx bxl-linkedin-square' ></i></a></p>
+			<p><a target="_blank" aria-label="Anchor to transfer to the company Instagram page" href="https://www.instagram.com/zielona_laska/" rel="noopener noreferrer"><i class='bx bxl-instagram-alt'></i></a></p>
+			<p><a target="_blank" aria-label="Anchor to transfer to the company Github page" href="https://github.com/blackglasses4/mywebsite" rel="noopener noreferrer"><i class='bx bxl-github' ></i></a></p>
 		</div>
 	</div>
 </footer>
