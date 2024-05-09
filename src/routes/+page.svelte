f218cac23c146c0b93eee7222d8c056f59275c3e
Adrianna Pawłowska <blackglassespx@gmail.com>
2024-05-09 21:18:45 +0200

    zmiany lekkie

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,20 +6,21 @@
 
     import { navigateToPreferredLanguageVersion } from '$lib/i18n/utils';
     
-    navigateToPreferredLanguageVersion();
+    //navigateToPreferredLanguageVersion();
 </script>
 
 <header id="header">
     <!-- <a href="/"><img class="logo" src="/image/logo.png" alt="logo firmy Telmech"/></a> -->
 </header>
 <body>
-	<p class="language-version-p">Wybierz wersję językową strony / Select language version page</p>
-	<section class="language-version">
-		<div class="lang-pl">
-			<p><img src="/flag/32/en.png" alt='English' /><a href="/en" rel="internal">Angielski</a></p>
-		</div>
-		<div class="lang-en">
-			<p><img src="/flag/32/pl.png" alt='Polski' /><a href="/pl" rel="internal">Polski</a></p>
+	<section class="section_preloader">
+		<p class="section_preloader-p">Wybierz wersję językową strony / Select language version page</p>
+		<div class="section_preloader-language">
+			<div class="lang-pl">
+				<p><img src="/flag/32/en.png" alt='English' /><a href="/en" rel="internal">Angielski</a></p>
+			<div class="lang-en">
+				<p><img src="/flag/32/pl.png" alt='Polski' /><a href="/pl" rel="internal">Polski</a></p>
+			</div>
 		</div>
 	</section>
 </body>
